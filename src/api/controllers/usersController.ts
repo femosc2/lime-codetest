6da746be6acca640ae310c554b82d33793bdb9cd
Felix Morau <felixmorau@gmail.com>
2020-04-25 22:46:24 +0200

    complete getUsers method

--- src/api/controllers/usersController.ts
+++ src/api/controllers/usersController.ts
@@ -2,10 +2,9 @@ import { Request, Response } from 'express';
 import { getUsersFromSchedule } from '../../services/scheduleReader';
 
 export const getUsers = (req: Request, res: Response) => {
-  getUsersFromSchedule();
-  res.send({
-    users:[
-    {id: 1, name: 'How to train your dragon' },
-    {id: 2, name: 'Queen of Katwe'},
-    ]
-  })};
+  const users = getUsersFromSchedule();
+  users.then((users) => {
+    res.send({
+      users
+    })})
+}
